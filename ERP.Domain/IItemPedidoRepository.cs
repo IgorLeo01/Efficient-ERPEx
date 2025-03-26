b3cb8d776fbf9725acaeb0def4814f54de3a0ef8
IgorLeo01 <igor.mourasilva@upe.br>
2025-03-26 19:37:30 -0300

    Feat: repositories to persist entity data

---
+++ ERP.Domain/IItemPedidoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Domain
+{
+    public interface IItemPedidoRepository
+    {
+        Task<IEnumerable<ItemPedido>> GetAllAsync();
+        Task<ItemPedido> GetByIdAsync(int id);
+        Task AddAsync(ItemPedido itemPedido);
+        Task UpdateAsync(ItemPedido itemPedido);
+        Task DeleteAsync(int id);
+    }
+}
