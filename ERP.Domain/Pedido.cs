42595932533cfa9c7a7953d2eb075210f285332b
IgorLeo01 <igor.mourasilva@upe.br>
2025-03-26 14:49:06 -0300

    Feat: creating domains

---
+++ ERP.Domain/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+        public DateTime DataPedido { get; set; } = DateTime.UtcNow;
+        public List<ItemPedido> Itens { get; set; } = new();
+        public double Total => Itens.Sum(i => i.Quantidade * i.Produto.Preco);
+    }
+}
